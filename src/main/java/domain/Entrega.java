c112212541bcbb3a76f4d46d2c348289eee9bdf4
juniorbarbosa <jc.barbosa.junior@hotmail.com>
2016-08-11 22:29:57 -0300

    #3 Criada classes Venda e Entrega.

---
+++ src/main/java/domain/Entrega.java
@@ -0,0 +1,35 @@
+package domain;
+
+import java.io.Serializable;
+
+public class Entrega implements Serializable {
+
+	private int idEntrega;
+	private Venda venda;
+	private String endereco;
+
+	public int getIdEntrega() {
+		return idEntrega;
+	}
+
+	public void setIdEntrega(int idEntrega) {
+		this.idEntrega = idEntrega;
+	}
+
+	public Venda getVenda() {
+		return venda;
+	}
+
+	public void setVenda(Venda venda) {
+		this.venda = venda;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+}
---
+++ src/main/java/domain/Venda.java
@@ -0,0 +1,45 @@
+package domain;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class Venda implements Serializable {
+
+	private int idVenda;
+	private String nomeCliente;
+	private double valor;
+	private List<String> itens;
+
+	public int getIdVenda() {
+		return idVenda;
+	}
+
+	public void setIdVenda(int idVenda) {
+		this.idVenda = idVenda;
+	}
+
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+
+	public double getValor() {
+		return valor;
+	}
+
+	public void setValor(double valor) {
+		this.valor = valor;
+	}
+
+	public List<String> getItens() {
+		return itens;
+	}
+
+	public void setItens(List<String> itens) {
+		this.itens = itens;
+	}
+
+}
