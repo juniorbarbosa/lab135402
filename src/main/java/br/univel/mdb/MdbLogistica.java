8c8af2ee3808594b93b68756a6d50e47b53bf2cc
juniorbarbosa <jc.barbosa.junior@hotmail.com>
2016-08-13 14:22:25 -0300

    #7 Criado MdbLogistica que ao receber a mensagem vai informar no console que recebeu uma mensagem, aguarda 30 segundos e então informar que a entrega foi concluida.

---
+++ src/main/java/br/univel/mdb/MdbLogistica.java
@@ -0,0 +1,46 @@
+package br.univel.mdb;
+
+import java.util.logging.Logger;
+
+import javax.ejb.ActivationConfigProperty;
+import javax.ejb.MessageDriven;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageListener;
+import javax.jms.ObjectMessage;
+
+import br.univel.domain.Venda;
+
+@MessageDriven(name = "MdbLogistica", activationConfig = {
+		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "queue/QueuePedido"),
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
+		@ActivationConfigProperty(propertyName = "maxSession", propertyValue = "1") })
+public class MdbLogistica implements MessageListener {
+
+	private final static Logger LOGGER = Logger.getLogger(MdbLogistica.class.toString());
+
+	@Override
+	public void onMessage(Message receivedMsg) {
+		ObjectMessage msg = null;
+		try {
+			if (receivedMsg instanceof ObjectMessage) {
+				msg = (ObjectMessage) receivedMsg;
+				Venda venda = (Venda) msg.getObject();
+				LOGGER.info("Objeto recebido: " + venda);
+
+				LOGGER.info("Mensagem recebida");
+				try {
+					Thread.sleep(30000);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				LOGGER.info("Entrega despachada");
+			} else {
+				LOGGER.warning("Objeto do tipo errado: " + receivedMsg.getClass().getName());
+			}
+		} catch (JMSException e) {
+			e.printStackTrace();
+		}
+	}
+}
--- src/main/java/br/univel/servlet/Config.java
+++ src/main/java/br/univel/servlet/Config.java
@@ -1,12 +1,53 @@
 package br.univel.servlet;
 
+import java.io.IOException;
+import javax.annotation.Resource;
+import javax.inject.Inject;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
 import javax.jms.JMSDestinationDefinition;
 import javax.jms.JMSDestinationDefinitions;
+import javax.jms.ObjectMessage;
+import javax.jms.Queue;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import br.univel.domain.Venda;
 
 @JMSDestinationDefinitions(value = {
 		@JMSDestinationDefinition(name = "java:/queue/QueuePedido", interfaceName = "javax.jms.Queue", destinationName = "QueuePedido"),
 		@JMSDestinationDefinition(name = "java:/topic/TopicVenda", interfaceName = "javax.jms.Topic", destinationName = "TopicVenda") })
+@WebServlet("/Config")
+public class Config extends HttpServlet {
 
-public class Config {
+	private static final long serialVersionUID = 1L;
+
+	@Inject
+	private JMSContext context;
+
+	@Resource(lookup = "java:/queue/QueuePedido")
+	private Queue queue;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			final Destination destination = queue;
+			Venda venda = new Venda();
+			ObjectMessage objectMessage = context.createObjectMessage();
+			objectMessage.setObject(venda);
+			context.createProducer().send(destination, objectMessage);
+
+		} catch (Throwable th) {
+			th.printStackTrace();
+		}
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
 
 }
