7e66fdad1c0fc711a721db503a409a9bb0be5631
juniorbarbosa <jc.barbosa.junior@hotmail.com>
2016-08-16 20:45:31 -0300

    #BUG Criada classes com queue em vez de topic conforme informado

--- src/main/java/br/univel/mdb/MdbAuditoria.java
+++ src/main/java/br/univel/mdb/MdbAuditoria.java
@@ -10,8 +10,8 @@ import javax.jms.ObjectMessage;
 import br.univel.domain.Venda;
 
 @MessageDriven(name = "MdbAuditoria", activationConfig = {
-		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "queue/QueuePedido"),
-		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "topic/TopicVenda"),
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
 		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
 		@ActivationConfigProperty(propertyName = "maxSession", propertyValue = "1") })
 public class MdbAuditoria implements MessageListener {
--- src/main/java/br/univel/mdb/MdbLog.java
+++ src/main/java/br/univel/mdb/MdbLog.java
@@ -10,8 +10,8 @@ import javax.jms.ObjectMessage;
 import br.univel.domain.Venda;
 
 @MessageDriven(name = "MdbLog", activationConfig = {
-		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "queue/QueuePedido"),
-		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Queue"),
+		@ActivationConfigProperty(propertyName = "destinationLookup", propertyValue = "topic/TopicVenda"),
+		@ActivationConfigProperty(propertyName = "destinationType", propertyValue = "javax.jms.Topic"),
 		@ActivationConfigProperty(propertyName = "acknowledgeMode", propertyValue = "Auto-acknowledge"),
 		@ActivationConfigProperty(propertyName = "maxSession", propertyValue = "1") })
 public class MdbLog implements MessageListener {
